115d0d232f7e7e1e79a71901fa520a38e1a39805
laharireddyk <43647490+laharireddyk@users.noreply.github.com>
2022-12-16 20:19:18 -0600

    Add files via upload

---
+++ Fah2cel.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Fah2cel {
+	public static void main(String[] args)
+	{
+float fah=212;
+double cel=(fah - 32)*(0.5556);
+
+	System.out.println(cel);
+	}
+}
---
+++ Inch2met.java
@@ -0,0 +1,10 @@
+
+public class Inch2met {
+	public static void main(String[] args)
+	{
+float inch=1000;
+double met=inch*0.0254;
+
+	System.out.println(met);
+	}
+}
